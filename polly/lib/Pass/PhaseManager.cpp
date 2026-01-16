8d3ebd2b0c65de03f83d69edb5d2bada95b531b6
johnkyky <ugo.battiston@protonmail.com>
2026-01-16 16:29:53 +0100

    fix bug with empty assumption cache

--- polly/lib/Pass/PhaseManager.cpp
+++ polly/lib/Pass/PhaseManager.cpp
@@ -218,7 +218,7 @@ public:
       ViewScops("scopsonly", true);
 
     // Phase: scops
-    AssumptionCache &AC = FAM.getResult<AssumptionAnalysis>(F);
+    AssumptionCache AC = AssumptionAnalysis().run(F, FAM);
     const DataLayout &DL = F.getParent()->getDataLayout();
     ScopInfo Info(DL, SD, SE, LI, AA, DT2, AC, ORE);
     if (Opts.isPhaseEnabled(PassPhase::PrintScopInfo)) {
