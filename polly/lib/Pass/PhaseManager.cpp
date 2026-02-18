bd715fe2aa6bbf9b9e3024d9cdbe5886029bc33b
johnkyky <ugo.battiston@protonmail.com>
2026-02-18 16:47:08 +0100

    test reg2mem

--- polly/lib/Pass/PhaseManager.cpp
+++ polly/lib/Pass/PhaseManager.cpp
@@ -53,6 +53,7 @@
 #include "llvm/Transforms/Scalar/LoopRotation.h"
 #include "llvm/Transforms/Scalar/SimplifyCFG.h"
 #include "llvm/Transforms/Utils/LoopUtils.h"
+#include "llvm/Transforms/Utils/Mem2Reg.h"
 
 #define DEBUG_TYPE "polly-pass"
 
@@ -363,6 +364,19 @@ public:
         Info.recompute();
       }
     }
+
+    auto PA = InstCombinePass().run(F, FAM);
+    FAM.invalidate(F, PA);
+    PA = SimplifyCFGPass().run(F, FAM);
+    FAM.invalidate(F, PA);
+
+    PA = PromotePass().run(F, FAM);
+    FAM.invalidate(F, PA);
+
+    PA = InstCombinePass().run(F, FAM);
+    FAM.invalidate(F, PA);
+    PA = SimplifyCFGPass().run(F, FAM);
+    FAM.invalidate(F, PA);
 
     return ModifiedIR;
   }
